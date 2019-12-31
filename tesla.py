f7826657de381eb3beab81b6783d2717dff0ab19
rtenacity <rtenacity@gmail.com>
2019-12-31 14:59:42 -0500

    I added ultrasound instead of ir and did a whole lot of stuff

--- tesla.py
+++ tesla.py
@@ -1,28 +1,70 @@
 #!/usr/bin/env python3
-from ev3dev2.motor import OUTPUT_B, OUTPUT_C, MoveTank
-from ev3dev2.sensor.lego import TouchSensor, InfraredSensor
+from ev3dev2.motor import OUTPUT_B, OUTPUT_C, MoveTank, MediumMotor, OUTPUT_A, MoveSteering
+from ev3dev2.sensor.lego import TouchSensor, InfraredSensor, UltrasonicSensor
 from sys import stderr
+from time import sleep
+from random import randint
 ts = TouchSensor()
-ir = InfraredSensor()
+#ir = InfraredSensor()
+
+ultra = UltrasonicSensor()
+mediumMotor = MediumMotor(OUTPUT_A)
 tankPair = MoveTank(OUTPUT_B, OUTPUT_C)
 
-def top_left_channel_1_action(state):
-    if state: # if state == True (pressed)
-        tankPair.off() 
-        exit()
-    else:
-        pass
+def backup(rot):
+    tankPair.on_for_rotations(left_speed = -50, right_speed = -50, rotations=rot)
+
+def moveForward(rot):
+    tankPair.on_for_rotations(left_speed = 50, right_speed = 50, rotations=rot)
+
+def turnLeft(rot):
+    tankPair.on_for_rotations(left_speed = 50, right_speed = -50, rotations=rot)
+
+def turnRight(rot):
+    tankPair.on_for_rotations(left_speed = -50, right_speed = 50, rotations=rot)
+
+def turnNeck(degrees):
+    mediumMotor.on_for_degrees(speed=30, degrees = degrees)
+
 
 print ("Starting....", file=stderr)
-ir.on_channel1_top_left = top_left_channel_1_action
+
 while True:
     try:
         tankPair.on(left_speed = 50, right_speed = 50) 
-        if (ts.is_pressed == True) or (ir.proximity <= 25):
-            tankPair.on_for_rotations(left_speed = -50, right_speed = -50, rotations=3)
-            tankPair.on_for_degrees(left_speed = -50, right_speed = 50, degrees = 360)
-    
-        ir.process()
+        if (ts.is_pressed == True):
+            tankPair.off()    
+            turnNeck(90)
+            sleep(.5) 
+            blockDistance = ultra.distance_centimeters
+            print(blockDistance)
+            if blockDistance > 100:
+                print("GO!", file= stderr)
+                turnNeck(-90)
+                backup(1)
+                turnLeft(1)
+
+            else:
+                turnNeck(-180)
+                sleep(1)
+                if ultra.distance_centimeters > 100:
+                    turnNeck(90)
+                    backup(1)
+                    turnRight(1)  
+                else:
+                    turnNeck(90)
+                    backup(2)
+                    if randint(1,2) == 1:
+                        turnRight(1)
+                    else:
+                        turnLeft(1)
+
+
+
+                     
+                    
+                   
+
     except KeyboardInterrupt:
         tankPair.off()
         exit()
